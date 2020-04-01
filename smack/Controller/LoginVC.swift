8d3769ead97998a6bc6384e433a119018d6d754e
Azim Jiwani <azimjiwani@hotmail.com>
2020-03-31 20:35:49 -0400

    added login page

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Azim Jiwani on 2020-03-31.
+//  Copyright © 2020 Azim Jiwani. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
